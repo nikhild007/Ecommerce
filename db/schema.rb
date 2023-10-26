87fedebe89017c90849a1fae5e22dff2b8673d6b
Nikhil Dash <nikhild@mindfiresolutions.com>
2023-10-26 17:33:00 +0530

    Order Creation and Cleanups from last PR,and views for Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_26_055111) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_090526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_26_055111) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "total"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
