3da208447fd18d778b9b86b0a8d86b23a1eaafe3
Nikhil Dash <nikhild@mindfiresolutions.com>
2023-10-30 20:47:10 +0530

    Add admin action for orders,create user policy,and views for admin orders page

--- app/policies/order_policy.rb
+++ app/policies/order_policy.rb
@@ -1,7 +1,7 @@
 class OrderPolicy < ApplicationPolicy
   attr_reader :user, :order
 
-  def initialize(user, order)
+  def initialize(user,order)
         @user = user
         @order = order
   end
@@ -11,6 +11,14 @@ class OrderPolicy < ApplicationPolicy
   end
 
   def destroy?
+    user.has_role?("admin")
+  end
+
+  def update?
+    user.has_role?("admin")
+  end
+
+  def get?
     user.has_role?("admin")
   end
 end
